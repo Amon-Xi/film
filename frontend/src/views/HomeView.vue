8c2ae0163cca7ce17053201890f31dbdb2e9c191
Amon-Xi <kb3315182@gmail.com>
2023-04-18 13:11:04 +0200

    mükszik

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -2,7 +2,7 @@
   <div> 
     <div class="p-3 d-flex col-4 ">
 
-
+      
         <input class="form-control me-2 " type="search" placeholder="Keress egy filmre..." aria-label="Search">
         <button class="btn btn-outline-danger d-flex" type="submit">Keresés</button>
     </div>
